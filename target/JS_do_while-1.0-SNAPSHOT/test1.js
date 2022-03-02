8aa07a0b2e764f8b7f637c1cfaffc2bbcc88d796
毛 <1296193245@qq.com>
2022-03-02 20:58:15 +0800

    JS_do_while

---
+++ target/JS_do_while-1.0-SNAPSHOT/test1.js
@@ -0,0 +1,19 @@
+/**
+ * Project name(项目名称)：JS_do_while
+ * File name(文件名): test1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/2
+ * Time(创建时间)： 20:55
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+var i = 1;
+do
+{
+    document.write(i + " ");
+    i++;
+}
+while (i > 5);
